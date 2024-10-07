885a77ff565ec3c4214014d7183bf24a9d137d18
ntkimyen <ntkimyen2711@gmail.com>
2024-10-07 22:21:26 +0700

    REST API Recomment system

--- frontend/home_content.php
+++ frontend/home_content.php
@@ -1,127 +1,162 @@
+<?php
+// Start the session and ensure it's not already started
+if (session_status() === PHP_SESSION_NONE) {
+    session_start();
+}
+
+// Fetch customer_id from session
+$customer_id = isset($_SESSION['customer_id']) ? $_SESSION['customer_id'] : null;
+
+// Define the API URL
+$api_url = "http://127.0.0.1:5000/recommend/{$customer_id}";
+
+$recommended_products = [];
+
+// Fetch the recommended products from the API
+if ($customer_id) {
+    $response = @file_get_contents($api_url); // Suppress warnings for failed requests
+
+    if ($response !== false) {
+        $recommended_products = json_decode($response, true); // Decode JSON response
+    } else {
+        echo "Error fetching recommendations.";
+    }
+}
+?>
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
-	<meta charset="UTF-8">
-	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<link rel="stylesheet" href="./css/style.css">
-	<title>Trang Chủ</title>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./css/style.css">
+    <title>Trang Chủ</title>
 </head>
+
 <body>
-<div class="custom-wrapper">
-	<div class="row row2">
-		<div class="col-md-8 col-xs-6 column-spacing">
-			<div class="slideshow-container">
-				<div class="mySlides1">
-					<img src="./img/banner/banner1.png" class="img-fluid">
-				</div>
+    <div class="custom-wrapper">
+        <div class="row row2">
+            <div class="col-md-8 col-xs-6 column-spacing">
+                <div class="slideshow-container">
+                    <div class="mySlides1">
+                        <img src="./img/banner/banner1.png" class="img-fluid">
+                    </div>
 
-				<div class="mySlides1">
-					<a href="./index.php?act=product&id=1028"><img src="./img/banner/banner2.png" class="img-fluid"></a>
-				</div>
+                    <div class="mySlides1">
+                        <a href="./index.php?act=product&id=1028"><img src="./img/banner/banner2.png"
+                                class="img-fluid"></a>
+                    </div>
 
-				<div class="mySlides1">
-					<a><img src="./img/banner/banner3.png" class="img-fluid"></a>
-				</div>
+                    <div class="mySlides1">
+                        <a><img src="./img/banner/banner3.png" class="img-fluid"></a>
+                    </div>
 
-				<div class="mySlides1">
-					<a href="./index.php?act=category&id=3"><img src="./img/banner/banner4.png" class="img-fluid"></a>
-				</div>
+                    <div class="mySlides1">
+                        <a href="./index.php?act=category&id=3"><img src="./img/banner/banner4.png"
+                                class="img-fluid"></a>
+                    </div>
 
-				<a class="prev" onclick="plusSlides(-1)">❮</a>
-				<a class="next" onclick="plusSlides(1)">❯</a>
+                    <a class="prev" onclick="plusSlides(-1)">❮</a>
+                    <a class="next" onclick="plusSlides(1)">❯</a>
 
-				<div class="dot-title">
-					<span class="dot1"></span>
-					<span class="dot1"></span>
-					<span class="dot1"></span>
-					<span class="dot1"></span>
-				</div>
-			</div>
-		</div>
+                    <div class="dot-title">
+                        <span class="dot1"></span>
+                        <span class="dot1"></span>
+                        <span class="dot1"></span>
+                        <span class="dot1"></span>
+                    </div>
+                </div>
+            </div>
 
-		<div class="col-md-4 col-xs-6 column-spacing-right">
-				<img src="./img/banner/bannerX.png" class="img-fluid" style="width:100%;">
-		</div>
-	</div>
-	<script src="./js/amination.js"></script>
-</div>
-<!-- SECTION -->
-<!-- <div class="section"> -->
-			<!-- container -->
-			<div class="container" style="width: 85%">
-				<!-- row -->
-				<div class="row">
-					<!-- shop -->
-					<div class="col-md-4 col-xs-6">
-						<div class="shop">
-							<div class="shop-img">
-								<img src="./img/banner/banner-right.png" alt="">
-							</div>
-							<div class="shop-body">
-								<a href="news.php"> <h3>Tin tức<br>Thực phẩm</h3> </a>
-							</div>
-						</div>
-					</div>
-					<!-- /shop -->
+            <div class="col-md-4 col-xs-6 column-spacing-right">
+                <img src="./img/banner/bannerX.png" class="img-fluid" style="width:100%;">
+            </div>
+        </div>
+        <script src="./js/amination.js"></script>
+    </div>
+    <!-- SECTION -->
+    <!-- <div class="section"> -->
+    <!-- container -->
+    <div class="container" style="width: 85%">
+        <!-- row -->
+        <div class="row">
+            <!-- shop -->
+            <div class="col-md-4 col-xs-6">
+                <div class="shop">
+                    <div class="shop-img">
+                        <img src="./img/banner/banner-right.png" alt="">
+                    </div>
+                    <div class="shop-body">
+                        <a href="news.php">
+                            <h3>Tin tức<br>Thực phẩm</h3>
+                        </a>
+                    </div>
+                </div>
+            </div>
+            <!-- /shop -->
 
-					<!-- shop -->
-					<div class="col-md-4 col-xs-6">
-						<div class="shop">
-							<div class="shop-img">
-								<img src="./img/banner/banner-between.png" alt="">
-							</div>
-						<div class="shop-body">
-							<a href="thuonghieu.php"> <h3>Trái cây<br>dinh dưỡng</h3> </a>
-						</div>
-					</div>
-				</div>
-					<!-- /shop -->
+            <!-- shop -->
+            <div class="col-md-4 col-xs-6">
+                <div class="shop">
+                    <div class="shop-img">
+                        <img src="./img/banner/banner-between.png" alt="">
+                    </div>
+                    <div class="shop-body">
+                        <a href="thuonghieu.php">
+                            <h3>Trái cây<br>dinh dưỡng</h3>
+                        </a>
+                    </div>
+                </div>
+            </div>
+            <!-- /shop -->
 
-					<!-- shop -->
-					<div class="col-md-4 col-xs-6">
-						<div class="shop">
-							<div class="shop-img">
-								<img src="./img/banner/banner-left.png" alt="">
-							</div>
-							<div class="shop-body">
-								<a href="Lienhe.php"><h3>Mỗi ngày<br>một món ăn</h3></a>
-							</div>
-						</div>
-					</div>
-					<!-- /shop -->
-				</div>
-				<!-- /row -->
-			</div>
-			<!-- /container -->
+            <!-- shop -->
+            <div class="col-md-4 col-xs-6">
+                <div class="shop">
+                    <div class="shop-img">
+                        <img src="./img/banner/banner-left.png" alt="">
+                    </div>
+                    <div class="shop-body">
+                        <a href="Lienhe.php">
+                            <h3>Mỗi ngày<br>một món ăn</h3>
+                        </a>
+                    </div>
+                </div>
+            </div>
+            <!-- /shop -->
+        </div>
+        <!-- /row -->
+    </div>
+    <!-- /container -->
 
-			 <!-- Banner -->
+    <!-- Banner -->
     <!-- /Banner -->
-		</div>
-		<!-- /SECTION -->
-        <!-- SECTION -->
-		<div class="section">
-			<!-- container -->
-			<div class="container">
-				<!-- row -->
-				<div class="row">
+    </div>
+    <!-- /SECTION -->
+    <!-- SECTION -->
+    <div class="section">
+        <!-- container -->
+        <div class="container">
+            <!-- row -->
+            <div class="row">
 
-					<!-- section title -->
-					<div class="col-md-12">
-						<div class="section-title text-md-left text-center">
-							<h3 class="title">Sản Phẩm</h3>
-						</div>
-					</div>
-					<!-- /section title -->
+                <!-- section title -->
+                <div class="col-md-12">
+                    <div class="section-title text-md-left text-center">
+                        <h3 class="title">Sản Phẩm</h3>
+                    </div>
+                </div>
+                <!-- /section title -->
 
-					<!-- Products tab & slick -->
-					<div class="col-md-12">
-						<div class="row">
-							<div class="products-tabs">
-								<!-- tab -->
-								<div id="tab1" class="tab-pane active">
-									<div class="products-slick" data-nav="#slick-nav-1">
-                                        <!-- product -->
-                                        <?php
+                <!-- Products tab & slick -->
+                <div class="col-md-12">
+                    <div class="row">
+                        <div class="products-tabs">
+                            <!-- tab -->
+                            <div id="tab1" class="tab-pane active">
+                                <div class="products-slick" data-nav="#slick-nav-1">
+                                    <!-- product -->
+                                    <?php
                                         $sql='select * from sanpham where 1 limit 4, 5';
                                         $list=executeResult($sql);
                                         foreach($list as $item){	
@@ -193,39 +228,145 @@
 										</div>';
                                         }
                                         ?>
-										<!-- /product -->
-										
-									</div>
-									<div id="slick-nav-1" class="products-slick-nav"></div>
-								</div>
-								<!-- /tab -->
-							</div>
-						</div>
-					</div>
-					<!-- Products tab & slick -->
-				</div><br><br>
-				<!-- /row -->
+                                    <!-- /product -->
 
-				<div class="row">
+                                </div>
+                                <div id="slick-nav-1" class="products-slick-nav"></div>
+                            </div>
+                            <!-- /tab -->
+                        </div>
+                    </div>
+                </div>
+                <!-- Products tab & slick -->
+            </div><br><br>
+            <!-- /row -->
 
-				<!-- section title -->
-				<div class="col-md-12">
+            <div class="row">
+
+                <!-- section title -->
+                <!-- <div class="col-md-12">
 					<div class="section-title text-md-left text-center">
 						<h3 class="title">Gợi ý hôm nay</h3>
 					</div>
-				</div>
-				<!-- /section title -->
+				</div> -->
+                <?php
+// Bắt đầu session
+if (session_status() == PHP_SESSION_NONE) {
+    session_start();
+}
 
-				<!-- Products tab & slick -->
-				<div class="col-md-12">
-				</div>
-				<!-- Products tab & slick -->
-				</div>
-			</div>
-			<!-- /container -->
-		</div><br><br>
-		<!-- /SECTION -->
+// In ra nội dung của session để kiểm tra
+// print_r($_SESSION);
+
+// Kiểm tra nếu khách hàng đã đăng nhập
+if (isset($_SESSION['id'])) {
+    // Lấy ID khách hàng từ session
+    $customer_id = $_SESSION['id'];
+
+    echo 'Customer ID: ' . $customer_id;
+
+    // Số lượng sản phẩm muốn hiển thị (10 sản phẩm)
+    $n_recommendations = 10;
+
+    // Tạo URL để gọi API gợi ý sản phẩm
+    $api_url = "http://127.0.0.1:5000/recommend/{$customer_id}"; // Thay $id bằng $customer_id
+
+    // Gọi API gợi ý sản phẩm bằng file_get_contents
+    $response = file_get_contents($api_url);
+
+    // Kiểm tra xem gọi API có thành công hay không
+    if ($response === FALSE) {
+        echo 'Gọi API thất bại!';
+    } else {
+        // Chuyển đổi dữ liệu JSON từ API thành mảng PHP
+        $recommendations = json_decode($response, true);
+
+        // In ra phản hồi từ API để kiểm tra
+        // print_r($recommendations);
+
+        // Kiểm tra xem API trả về dữ liệu hợp lệ hay không
+        if (isset($recommendations['recommendations']) && count($recommendations['recommendations']) > 0) {
+            // Hiển thị danh sách sản phẩm gợi ý
+            echo '<div class="row">';
+            echo '  <div class="col-md-12">';
+            echo '      <div class="section-title text-md-left text-center">';
+            echo '          <h3 class="title">Sản phẩm gợi ý</h3>';
+            echo '      </div>';
+            echo '  </div>';
+            echo '  <div class="col-md-12">';
+            echo '      <div class="row">';
+            echo '          <div class="products-tabs">';
+            echo '              <div id="tab1" class="tab-pane active">';
+            echo '                  <div class="products-slick" data-nav="#slick-nav-1">';
+
+            // Duyệt qua các sản phẩm được gợi ý từ API
+            foreach ($recommendations['recommendations'] as $item) {
+                $gia_goc = $item['original_price'];
+                $don_gia = $item['price'];
+
+                // Tính phần trăm giảm giá nếu có
+                if ($gia_goc > $don_gia) {
+                    $phan_tram_giam = round((($gia_goc - $don_gia) / $gia_goc) * 100);
+                } else {
+                    $phan_tram_giam = 0;
+                }
+
+                // Hiển thị sản phẩm (tùy chỉnh theo thiết kế của bạn)
+                echo '<div class="product">';
+                echo '  <div class="product-img" style="height:250px">';
+                echo '      <img src="./img/' . htmlspecialchars($item['image']) . '" alt="" style="height:100%">';
+                echo '      <div class="product-label">';
+                if ($phan_tram_giam > 0) {
+                    echo '          <span class="new">-' . $phan_tram_giam . '%</span>';
+                }
+                echo '      </div>';
+                echo '  </div>';
+                echo '  <div class="product-body">';
+                echo '     <p class="product-category"><small>'.$item['sold_quantity'].' đã bán</small></p>';
+                echo '      <h3 class="product-name"><a href="?act=product&id=' . htmlspecialchars($item['product_id']) . '">' . htmlspecialchars($item['product_name']) . '</a></h3>';
+                echo '      <h4 class="product-price">' . currency_format($item['price']) . '</h4>';
+                echo '												<h4 class="product-pricece" id="price-sold">'.currency_format($item['original_price']).' </h4>  <div class="product-rating">
+														<i class="fa fa-star"></i>
+														<i class="fa fa-star"></i>
+														<i class="fa fa-star"></i>
+														<i class="fa fa-star"></i>
+														<i class="fa fa-star"></i>
+													</div></div>';
+                echo '  <div class="add-to-cart">';
+                echo '      <button class="add-to-cart-btn" onclick="addCart(' . htmlspecialchars($item['product_id']) . ', 1); themThanhCong(' . htmlspecialchars($item['product_id']) . ');"><i class="fa fa-shopping-cart"></i> <span>Thêm vào giỏ</span></button>';
+                echo '  </div>';
+                echo '</div>';
+            }
+
+            echo '                  </div>';
+            echo '                  <div id="slick-nav-1" class="products-slick-nav"></div>';
+            echo '              </div>';
+            echo '          </div>';
+            echo '      </div>';
+            echo '  </div>';
+            echo '</div>';
+        } else {
+            echo '<p>Không có sản phẩm gợi ý cho bạn vào lúc này.</p>';
+        }
+    }
+} else {
+    // Nếu khách hàng chưa đăng nhập
+    echo '<p>Vui lòng đăng nhập để xem sản phẩm gợi ý.</p>';
+}
+?>
+
+                <!-- /section title -->
+
+                <!-- Products tab & slick -->
+                <div class="col-md-12">
+                </div>
+                <!-- Products tab & slick -->
+            </div>
+        </div>
+        <!-- /container -->
+    </div><br><br>
+    <!-- /SECTION -->
+
 </body>
+
 </html>
-
-
--- frontend/xulydangnhap.php
+++ frontend/xulydangnhap.php
@@ -1,26 +1,27 @@
 <?php
-if (isset($_POST['dangnhap']))
-{
-
-    $conn = mysqli_connect('localhost', 'root', '', 'foodgennie') or die ('Lỗi kết nối'); mysqli_set_charset($conn, "utf8");
+if (isset($_POST['dangnhap'])) {
+    $conn = mysqli_connect('localhost', 'root', '', 'foodgennie') or die('Lỗi kết nối');
+    mysqli_set_charset($conn, "utf8");
     $username = addslashes($_POST['username']);
     $password = addslashes($_POST['password']);
 
-    if ($username == "" || $password =="") {
-        echo '<br><p style="color:red;">Username hoặc Password không được<br> để trống!</p>';
-    }else{
-        $sql = "select ten_dangnhap, mat_khau, trangthai from khachhang where ten_dangnhap = '$username' and mat_khau = '$password' ";
-        $query = mysqli_query($conn,$sql);
+    if ($username == "" || $password == "") {
+        echo '<br><p style="color:red;">Username hoặc Password không được để trống!</p>';
+    } else {
+        $sql = "SELECT id, ten_dangnhap, mat_khau, trangthai FROM khachhang WHERE ten_dangnhap = '$username' AND mat_khau = '$password'";
+        $query = mysqli_query($conn, $sql);
         $num_rows = mysqli_num_rows($query);
-        if ($num_rows==0) {
-            echo '<br><p style="color:red;">Tên đăng nhập hoặc mật khẩu<br> không đúng !</p>';
-        }else{
-            $row =mysqli_fetch_array($query, 1);
-            //tiến hành lưu tên đăng nhập vào session để tiện xử lý sau này
+
+        if ($num_rows == 0) {
+            echo '<br><p style="color:red;">Tên đăng nhập hoặc mật khẩu không đúng!</p>';
+        } else {
+            $row = mysqli_fetch_array($query, 1);
+            // Tiến hành lưu tên đăng nhập và ID vào session
             if ($row['trangthai'] == 1) {
                 echo "<script type='text/javascript'>alert('Tài khoản của bạn đã bị khóa');window.location='index.php?act=login';</script>";
-            }else {
+            } else {
                 $_SESSION['ten_dangnhap'] = $username;
+                $_SESSION['id'] = $row['id']; // Lưu ID khách hàng vào session
                 echo "<script type='text/javascript'>alert('Đăng nhập thành công');window.location='index.php';</script>";
             }
         }
